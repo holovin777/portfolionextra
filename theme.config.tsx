ac7def1e06b9be5c52de807f7778855fdce67aec
Viktor Holovin <holovin@mail.com>
2023-02-18 17:49:25 +0100

    Change docs link

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  project: {
+    link: 'https://github.com/holovin777/portfolionextra',
+  },
+  docsRepositoryBase: 'https://github.com/holovin777/portfolionextra/',
   logo: <span>Viktor Holovin</span>,
   footer: {
     text: 'Viktor Holovin',
